740a28881d7dd789a6f5d550118b6f7dc1244654
koekigamer <78793095+koekigamer@users.noreply.github.com>
2021-04-10 22:05:22 +0200

    af

--- dobbel.py
+++ dobbel.py
@@ -2,28 +2,27 @@ from tkinter import *
 import random
 window = Tk()
 
-window.geometry ("200x300")
+window.geometry ("300x300")
 
 andwoord = Label(window, text="")
+andwoord.grid(column = 1, row = 2)
 
 
-def show(event):
+def show():
     getal = random.randint(0, 6)
     ruimte = Label(window, text=getal)
     andwoord.configure(text=getal)
 
 
 label = Label()
-btn_submit = Button(master=label, text="dobbel", command = show) 
-btn_submit.place(x=25, y=100)
-btn_submit.pack()
-
+btn_submit = Button(master=window, text="dobbel", command = show) 
+btn_submit.grid(column = 1, row = 1)
 
 
 def dobbelen(event):
     print(getal)
 
-btn_submit.bind("<Button-1>", show)
 
 
-window = mainloop()
+
+window.mainloop()
